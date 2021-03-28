36cecb1db6c84c02647fff7a1bcdff1c9a4232bf
Candice Whiskey <candicewhiskey77@gmail.com>
2021-03-28 18:19:36 -0400

    set up js file variables

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var score =0;
+var banner =document.querySelector(".banner");
+var question =document.querySelector(".question");
+var topscore = document.querySelector(".topscore");
+var row = document.querySelector(".row");
+var correctanswer = document.querySelector(".correct");
+var wronganswer = document.querySelector(".wrong");
+var timer =document.querySelector(".timer");
+var timerInterval;
